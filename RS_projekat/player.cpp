553a5e767604f30936863d70069d247092f73537
MihaNov <mihaildnovakovic@gmail.com>
2016-05-08 15:33:50 +0200

    Dodata energija Igracu koja se smanjuje na pokrete. Kada opadne na 0 igrac umire

--- RS_projekat/player.cpp
+++ RS_projekat/player.cpp
@@ -7,7 +7,12 @@
 
 Player::Player(int x, int y) {
     this->setRect(x,y,5,5);
+    energy=10;                   //energija se ovde inicijalizuje, stavio sam 10 da bi lakse videli dal radi
 }
+
+Player::~Player() {
+}
+
 
 void Player::keyPressEvent(QKeyEvent *event){
     int modifier_x = 0;
@@ -15,13 +20,20 @@ void Player::keyPressEvent(QKeyEvent *event){
 
     if (event->key() == Qt::Key_A){
         modifier_x = -10;
+        energy=energy-1;                          //energija se smanjuje svakim pokretom igraca
     } else if (event->key() == Qt::Key_D){
         modifier_x = 10;
+        energy=energy-1;
     } else if (event->key() == Qt::Key_W){
         modifier_y = -10;
+        energy=energy-1;
     } else if (event->key() == Qt::Key_S){
         modifier_y = 10;
+        energy=energy-1;
     }
+
+    if(energy<=0)   //Ako igrac skroz izgubi energiju, nestaje
+        this->Player::~Player();
 
     this->setPos(x() + modifier_x, y() + modifier_y);
 
--- RS_projekat/player.h
+++ RS_projekat/player.h
@@ -4,9 +4,12 @@
 #include <QGraphicsRectItem>
 
 class Player: public QGraphicsRectItem{
+    private:
+        int energy;  //promenljiva koja pokazuje koliko igrac ima energije, kada padne na 0 , on umire
     public:
         Player(int x, int y);
         void keyPressEvent(QKeyEvent * event);
+        ~Player();
 };
 
 #endif // PLAYER_H
